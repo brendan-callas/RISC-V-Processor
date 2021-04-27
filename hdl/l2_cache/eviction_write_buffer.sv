5ca3693a1ca3caf9c98c688b3125977787230a3a
Christopher Shannon <cjs5@linux-35.ews.illinois.edu>
2021-04-27 03:49:11 -0500

    rewrote EWB to be asynchronous with reads/writes/etc

--- hdl/l2_cache/eviction_write_buffer.sv
+++ hdl/l2_cache/eviction_write_buffer.sv
@@ -5,11 +5,16 @@ module eviction_write_buffer
 	input rst,
 	
 	input logic load,
+	input logic [31:0] hit_addr,
+	
 	input logic [255:0] wdata_i, // from cache
 	input logic [31:0] address_i,
+	input logic empty,
 	
-	output logic [255:0] wdata_o, //to main memory
-	output logic [31:0] address_o
+	output logic [255:0] wdata_o, //to main memory and L1
+	output logic [31:0] address_o,
+	output logic full_o,
+	output logic hit_o
 	
 
 );
@@ -17,18 +22,26 @@ module eviction_write_buffer
 // internal registers
 logic [255:0] wdata;
 logic [31:0] address;
+logic [26:0] tag; //tag of data currently held
+logic [26:0] mem_addr_tag; //tag of incoming address to check for a hit
+logic full; //if EWB has valid data that still needs writing back
+logic hit;
+
+
 
 
 always_ff @(posedge clk)
 begin
-    if (rst) begin
+    if (empty | rst) begin
         wdata <= '0;
 		address <= '0;
+		full <= '0;
     end
     else begin
         if(load) begin
 			wdata <= wdata_i;
 			address <= address_i;
+			full <= 1'b1;
 		end
     end
 end
@@ -36,6 +49,16 @@ end
 always_comb begin
 	wdata_o = wdata;
 	address_o = address;
+	full_o = full;
+	hit_o = hit & full; //only have a hit if there is valid data
+	
+	tag = address[31:5];
+	mem_addr_tag = hit_addr[31:5]; //use more bits since it has no associated set
+	
+	if( (tag == mem_addr_tag) & full)
+		hit_o = 1'b1;
+	else hit_o = 1'b0;
+	
 end
 
 
--- hdl/l2_cache/l2_cache_control.sv
+++ hdl/l2_cache/l2_cache_control.sv
@@ -28,6 +28,10 @@ module l2_cache_control (
 	// signals for EWB
 	output logic load_ewb,
 	output logic evict_addr_sel,
+	output logic empty_ewb,
+	output logic ewb_wdata_sel,
+	input logic ewb_full,
+	input logic ewb_hit,
 	
 	//port to memory
 	input logic resp_from_mem,
@@ -35,16 +39,20 @@ module l2_cache_control (
 	output logic write_to_mem
 );
 
+// performance counters
+int num_hits;
+int num_misses;
+int num_writebacks;
+
+
 
 enum int unsigned {
     /* List of states */
 	s_idle,
+	s_wait_for_ewb,
 	s_write_back_to_ewb,
 	s_load_data_from_mem,
-	s_load_data_from_mem_ewb,
-	s_respond_to_cpu,
-	s_respond_to_cpu_ewb,
-	s_write_back_from_ewb
+	s_respond_to_cpu
 	
 } state, next_state;
 
@@ -62,7 +70,9 @@ function void set_defaults();
 	load_dirty_arr = 1'b0;
 	dirty_sel = plru_idx; // dirty output is only important for checking if we need to write back when evicting.
 	load_ewb = 1'b0;
-	evict_addr_sel = 1'b0; // default to address from cache
+	evict_addr_sel = 1'b0; // L2 Addr
+	empty_ewb = 1'b0;
+	ewb_wdata_sel = 1'b0; //cacheline data from L2
 endfunction
 
 always_comb
@@ -77,34 +87,24 @@ begin : state_actions
 			//all signals are default
 		end
 		
+		s_wait_for_ewb: begin
+			write_to_mem = 1'b1;
+			evict_addr_sel = 1'b1;
+			if(resp_from_mem)
+				empty_ewb = 1'b1;
+		end
+		
 		s_write_back_to_ewb: begin
 			way_sel = plru_idx;
 			tag_sel = 1'b0; // tag related to cacheline data
 			load_ewb = 1'b1;
 		end
 		
-		s_write_back_from_ewb: begin
-			evict_addr_sel = 1'b1; //use address from EWB
-			write_to_mem = 1'b1;
-		end
-		
 		s_load_data_from_mem: begin
 			read_from_mem = 1'b1;
 			way_sel = plru_idx;
 			tag_sel = 1'b1; //select mem addr tag
-			
-			if(resp_from_mem == 1'b1) begin
-				load_cache = 1'b1;
-				source_sel = 1'b1; // memory
-				load_dirty_arr = 1'b1; // load a 0 if reading (and evicting), load 1 if writing. 
-			end
-		end
-		
-		// control signals same (but next state is different)
-		s_load_data_from_mem_ewb: begin
-			read_from_mem = 1'b1;
-			way_sel = plru_idx;
-			tag_sel = 1'b1; //select mem addr tag
+			evict_addr_sel = 1'b0; //L2 Address
 			
 			if(resp_from_mem == 1'b1) begin
 				load_cache = 1'b1;
@@ -116,19 +116,20 @@ begin : state_actions
 		s_respond_to_cpu: begin
 			mem_resp = 1'b1;
 			way_sel = hit_idx; // redundant since this is default; keeping it here anyway for now
-			load_lru = 1'b1; // lru will load way_sel into the respective index
-			load_cache = mem_write; // want to load cache if we are writing;
-			if(mem_write) load_dirty_arr = 1'b1;
+			
+			if(ewb_hit) begin
+				ewb_wdata_sel = 1'b1; //wdata from L1
+				load_ewb = mem_write;
+			end
+			else begin
+				load_cache = mem_write; // want to load cache if we are writing;
+				load_lru = 1'b1; // lru will load way_sel into the respective index
+				if(mem_write) load_dirty_arr = 1'b1;
+			end
+			
+			
 		end
 		
-		// control signals same (but next state is different)
-		s_respond_to_cpu_ewb: begin
-			mem_resp = 1'b1;
-			way_sel = hit_idx; // redundant since this is default; keeping it here anyway for now
-			load_lru = 1'b1; // lru will load way_sel into the respective index
-			load_cache = mem_write; // want to load cache if we are writing;
-			if(mem_write) load_dirty_arr = 1'b1;
-		end
 	endcase
 	
 end
@@ -152,27 +153,30 @@ begin : next_state_logic
 		 case(state)
 		
 			s_idle: begin
-				if( (mem_read | mem_write) & ~cache_hit & dirty_o) begin
-					next_state = s_write_back_to_ewb;
+				if( (mem_read | mem_write) & ~(cache_hit | ewb_hit) & dirty_o) begin
+				
+					if(ewb_full)
+						next_state = s_wait_for_ewb;
+					else
+						next_state = s_write_back_to_ewb;
 				end
 				
-				else if( (mem_read | mem_write) & ~cache_hit & ~dirty_o) begin
+				else if( (mem_read | mem_write) & ~(cache_hit | ewb_hit) & ~dirty_o) begin
 					next_state = s_load_data_from_mem;
 				end
 				
-				if( (mem_read | mem_write) & cache_hit) begin
+				if( (mem_read | mem_write) & (cache_hit | ewb_hit) ) begin
 					next_state = s_respond_to_cpu;
 				end
 			end
 			
-			s_write_back_to_ewb: begin
-				next_state = s_load_data_from_mem_ewb; //loading EWB should only take 1 cycle
+			s_wait_for_ewb: begin
+				if(resp_from_mem == 1'b1)
+					next_state = s_write_back_to_ewb;
 			end
 			
-			s_write_back_from_ewb: begin
-				if(resp_from_mem == 1'b1) begin
-					next_state = s_idle;
-				end
+			s_write_back_to_ewb: begin
+				next_state = s_load_data_from_mem; //loading EWB should only take 1 cycle
 			end
 			
 			s_load_data_from_mem: begin
@@ -181,18 +185,8 @@ begin : next_state_logic
 				end
 			end
 			
-			s_load_data_from_mem_ewb: begin
-				if(resp_from_mem == 1'b1) begin
-					next_state = s_respond_to_cpu_ewb;
-				end
-			end
-			
 			s_respond_to_cpu: begin
 				next_state = s_idle;
-			end
-			
-			s_respond_to_cpu_ewb: begin
-				next_state = s_write_back_from_ewb;
 			end
 			
 		endcase
