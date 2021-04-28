03a68a6eb3efddce5cb598776cda88759a89e112
Prateek Tenkale <tenkale2@illinois.edu>
2021-04-27 23:30:02 -0700

    Initial OLB Prefetching

---
+++ hdl/instr_cache/i_reg_array.sv
@@ -0,0 +1,46 @@
+/* A register array without a ****ing delay */
+
+module i_reg_array #(
+    parameter s_index = 3,
+    parameter width = 1
+)
+(
+    clk,
+    rst,
+    load,
+    index,
+    datain,
+    dataout,
+    obl_dataout
+);
+
+localparam num_sets = 2**s_index;
+
+input clk;
+input rst;
+input load;
+input [s_index-1:0] index;
+input [width-1:0] datain;
+output logic [width-1:0] dataout;
+output logic [width-1:0] obl_dataout;
+
+logic [width-1:0] data [num_sets-1:0] /* synthesis ramstyle = "logic" */;
+
+always_ff @(posedge clk)
+begin
+    if (rst) begin
+        for (int i = 0; i < num_sets; ++i)
+            data[i] <= '0;
+    end
+    else begin
+        if(load)
+            data[index] <= datain;
+    end
+end
+
+always_comb begin
+	dataout = data[index];
+    obl_dataout = data[index+1];
+end
+
+endmodule : reg_array
---
+++ hdl/instr_cache/i_way.sv
@@ -0,0 +1,93 @@
+module i_way
+(
+    input clk,
+    input rst,
+	
+	input logic [2:0] index_i,
+	input logic [255:0] data_i,
+	input logic [31:0] byte_enable_i,
+	input logic load_i,
+	// input logic mem_write_i,
+	input logic [23:0] tag_i,
+	input logic read_cache_data_i,
+	// input logic load_dirty,
+
+	input logic load_busy,
+	input logic busy_i,
+	input logic [2:0] busy_index_i,
+
+	output logic [255:0] data_o,
+	
+	output logic [23:0] tag_o,
+	output logic valid_o,
+	output logic busy_o,
+
+	output logic [23:0] obl_tag_o,
+	output logic obl_valid_o,
+	output logic obl_busy_o,
+    
+);
+
+i_reg_array #(.s_index(3), .width(1)) valid_array(
+	.clk(clk),
+	.rst(rst),
+	.load(load_i),
+	.index(index_i),
+	.datain(1'b1),	// 1 because the valid bit can never change from 1 to 0 (I think).
+	.dataout(valid_o),
+	.obl_dataout(obl_valid_o)
+);
+
+i_reg_array #(.s_index(3), .width(1)) busy_array(
+	.clk(clk),
+	.rst(rst),
+	.load(load_busy),
+	.index(busy_index_i),
+	.datain(busy_i),
+	.dataout(busy_o),
+	.obl_dataout(obl_busy_o)
+);
+
+i_reg_array #(.s_index(3), .width(24)) tag_array(
+	.clk(clk),
+	.rst(rst),
+	.load(load_i),
+	.index(index_i),
+	.datain(tag_i),
+	.dataout(tag_o),
+	.obl_dataout(obl_tag_o)
+);
+
+data_array data_array(
+	.clk(clk),
+	.rst(rst),
+	.read(read_cache_data_i),
+	.write_en(byte_enable_i),
+	.rindex(index_i),
+	.windex(index_i),
+	.datain(data_i),
+	.dataout(data_o)
+);
+
+endmodule : way
+
+//********** Comparator ************//
+module comparator #(
+    parameter width = 24
+)
+(
+    input logic [width-1:0] a,
+	input logic [width-1:0] b,
+	output logic f
+);
+
+always_comb begin
+
+	if(a == b) begin
+		f = 1'b1;
+	end
+	else f = 1'b0;
+	
+end
+
+endmodule : comparator
